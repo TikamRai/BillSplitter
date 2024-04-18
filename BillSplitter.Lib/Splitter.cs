b732d2668f88a778ef54ba31ab1ca62193a7d7c5
Tikam Rai <a00280792@mycambrian.ca>
2024-04-18 04:52:35 +0000

    Class library and test project created and referenced from solution file. The class library was also referenced from the test project. Method to take in an amount and the number of people created, which returns the split amount.

---
+++ BillSplitter.Lib/Splitter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BillSplitter.Lib;
+
+public class Splitter
+{
+    public static decimal Split(decimal amount, int numberOfPeople)
+    {
+        if (numberOfPeople <= 0)
+        {
+            throw new ArgumentException("Number of people must be greater than zero.", nameof(numberOfPeople));
+
+        }
+
+        return Math.Round(amount / numberOfPeople, 2, MidpointRounding.AwayFromZero);
+    }
+}
