6850b0996b93a6abdc3faad3c633af8b137cbcf3
Tikam Rai <a00280792@mycambrian.ca>
2024-04-18 05:20:33 +0000

    Created a method with two arguments: a dictionary of <String, Decimal> and a float with appropriate logic

--- BillSplitter.Lib/Splitter.cs
+++ BillSplitter.Lib/Splitter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generics;
 
 namespace BillSplitter.Lib;
 
@@ -13,5 +14,22 @@ public class Splitter
         }
 
         return Math.Round(amount / numberOfPeople, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static Dictionary<string, decimal> CalcTips(Dictionary<string, decimal> mealCosts, float tipPercent)
+    {
+        if (tipPercent < 0)
+        {
+            throw new ArgumentException("Tip percentage cannot be negative.", nameof(tipPercent)); 
+        }
+
+        var tips = new Dictionary<string, decimal>();
+        foreach (var entry in mealCosts)
+        {
+            decimal tipAmount = Math.Round(entry.Value * ((decimal)tipPercent / 100), 2);
+            tips.Add(entry.Key, tipAmount);
+        }
+
+        return tips;
     }
 }
